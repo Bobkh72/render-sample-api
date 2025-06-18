2c2113cdeadd533f432ede8cb636288b7df9f240
Bobkh72 <100011071+Bobkh72@users.noreply.github.com>
2025-06-18 08:21:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from datetime import datetime
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {
+        "datetime": datetime.now().isoformat(),
+        "products": [
+            {"id": 1, "name": "Laptop", "price": 999.99},
+            {"id": 2, "name": "Smartphone", "price": 699.99},
+            {"id": 3, "name": "Headphones", "price": 199.99}
+        ]
+    }
