0873c4a6fda853630dbb578c2395d8f12ed9d362
Bobkh72 <100011071+Bobkh72@users.noreply.github.com>
2025-06-19 07:18:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ MONGO_URI = "mongodb+srv://bobkhoury72:DRhqkTqJFlRsBkRf@cluster0.uz5b1qf.mongodb
 client = MongoClient(MONGO_URI)
 
 # Select the database and collection
-db = client["your_db_name"]  # <-- Replace with your database name
-collection = db["your_collection_name"]  # <-- Replace with your collection name
+db = client["MainDataBase"]  # <-- Replace with your database name
+collection = db["walletHolders"]  # <-- Replace with your collection name
 
 @app.get("/items")
 def read_items():
