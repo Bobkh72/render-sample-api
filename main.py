985de87b700220f0a4c6282512771133985a4073
Bobkh72 <100011071+Bobkh72@users.noreply.github.com>
2025-06-19 18:05:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,25 @@ from fastapi import FastAPI
 from pymongo import MongoClient
 from bson.json_util import dumps
 from fastapi.responses import JSONResponse
-import os
 
 app = FastAPI()
 
-# MongoDB connection string (use environment variable in production)
 MONGO_URI = "mongodb+srv://bobkhoury72:DRhqkTqJFlRsBkRf@cluster0.uz5b1qf.mongodb.net/"
 client = MongoClient(MONGO_URI)
-
-# Select the database and collection
 db = client["MainDataBase"]
 collection = db["walletHolders"]
 
 @app.get("/items")
 def read_items():
     try:
-        items = list(collection.find())
+        items = list(collection.find({}, {
+            "_id": 0,  # exclude MongoDB internal ID
+            "holderName": 1,
+            "walletId": 1,
+            "walletNumber": 1,
+            "BalanceDB_LBP": 1,
+            "BalanceDB_USD": 1
+        }))
         return JSONResponse(content=dumps(items), media_type="application/json")
     except Exception as e:
         return {"error": str(e)}
